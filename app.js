f00a352bcc52f41a7421a2a7a21dd1a64ad7d3ed
Sebastian Pallero <sebastianoria@hotmail.com>
2023-06-08 16:58:28 -0300

    ADD, express

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 import ProductManager from "./productManager.js";
-const path = "./db.json";
+import express from 'express'
+
+
+const path = "./db.json"
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.listen(8080, ()=> console.log('Listening on port 8080'))
+
+app.get('/products', async(req, res) =>{
+    let {limit} = req.query
+    let products = await manager.getProducts()
+    if(limit){
+        let filteredProducts = products.slice(0, limit)
+        res.send(filteredProducts)
+    }
+    else{
+        res.send(products)
+    }
+})
+
+app.get('/products/:pid', async(req, res) =>{
+    let productId = req.params.pid
+    let products = await manager.getProducts()
+    let filteredProduct = products.find(product => product.id == productId)
+    res.send({filteredProduct})
+})
+
 
 // Se Crea la instacia de ProductManager
-const manager = new ProductManager(path);
+const manager = new ProductManager(path)
 
 //DESCOMENTAR LAS FUNCIONES PARA PROBAR LOS METODOS
 
@@ -10,16 +37,16 @@ const manager = new ProductManager(path);
 
 const addProduct = async() => {
     await manager.addProduct({
-        title: "Manzana",
-        description: "Manzana Roja de Río Negro",
-        price: 60,
+        title: "Uva",
+        description: "Uva de Mendoza",
+        price: 90,
         thumbnail: "https://images.app.goo.gl/VVHm5zYYH6NZjQSP7",
-        code: "1",
+        code: "4",
         stock: 20,
     });
 }
 
-/* addProduct();  */ //DESCOMENTAR
+/*  addProduct(); */ //DESCOMENTAR
  
 
 // GET ALL PRODUCTS
@@ -28,7 +55,7 @@ const getProducts = async() =>{
     console.log(await manager.getProducts());
 }
 
-/* getProducts(); */ //DESCOMENTAR
+ /* getProducts(); */ //DESCOMENTAR
 
 
 // GET PRODUCT BY ID
